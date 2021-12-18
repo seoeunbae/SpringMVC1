4834ff9763d37821c0571577599b7e079f1428ee
SeoeunBae <enyoupark@gmail.com>
2021-12-18 16:13:15 +0900

    서블릿, jsp, mvc 패턴 전까지

--- src/main/java/hello/servlet/ServletInitializer.java
+++ src/main/java/hello/servlet/ServletInitializer.java
@@ -2,7 +2,7 @@ package hello.servlet;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//원래있는 클래스..
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
